2cb3b7b021648f5dd8df90c40edd88c0356e908f
SvetlanaIL <ilyasovasv91@yandex.ru>
2023-11-02 16:40:22 +0300

    add classnames, feature-slised design architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,23 @@
 import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {useTheme} from "./theme/useTheme";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import {classNames} from "shared/lib/classNames/classNames";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Toogle theme</button> <br/>
             <Link to={'/'}>Главная <br/></Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />}/>
-                    <Route path={'/'} element={<MainPageAsync />}/>
+                    <Route path={'/about'} element={<AboutPage />}/>
+                    <Route path={'/'} element={<MainPage />}/>
                 </Routes>
             </Suspense>
         </div>
