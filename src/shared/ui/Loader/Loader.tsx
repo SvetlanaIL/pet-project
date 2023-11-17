2180e7bb26d432b898a595ffb42fd68c2015980c
SvetlanaIL <ilyasovasv91@yandex.ru>
2023-11-17 15:57:33 +0300

    add 404 page, add loader

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,17 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Loader.module.scss';
+
+export interface LoaderProps {
+    className?: string,
+}
+
+export const Loader = ({className}: LoaderProps) => {
+    return (
+        <div className={classNames(cls.Loader, {}, [className])}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
