8a2b0abe1a89b069caacd33b1abee6eff7dceec7
SvetlanaIL <ilyasovasv91@yandex.ru>
2024-04-11 12:41:09 +0300

    add User entity

---
+++ src/entities/User/model/types/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: string,
+    username: string,
+}
+
+export interface UserSchema {
+    //Если authData = undefined, то пользователь не авторизован
+    authData?: User,
+}
