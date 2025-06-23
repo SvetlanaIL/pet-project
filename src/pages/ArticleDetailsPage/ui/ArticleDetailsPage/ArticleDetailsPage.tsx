d0815c4c604000f40e06aae2cd5a54f9ec3dee05
SvetlanaIL <ilyasovasv91@yandex.ru>
2025-06-23 15:44:26 +0300

    add skeleton, code, article details card

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -2,6 +2,8 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import cls from './ArticleDetailsPage.module.scss';
+import {useParams} from "react-router-dom";
+import {ArticleDetails} from "entities/Article";
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -10,10 +12,19 @@ interface ArticleDetailsPageProps {
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     const { className } = props;
     const { t } = useTranslation();
+    const { id } = useParams<{ id: string }>();
+
+    if (!id) {
+        return (
+            <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+                {t('Статья не найдена')}
+            </div>
+        );
+    }
 
     return (
         <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-            ARTICLE DETAILS
+            <ArticleDetails id={id} />
         </div>
     );
 };
