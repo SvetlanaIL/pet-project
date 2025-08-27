e92a03f9a0ebd7308151f18fe765967c07f6f73a
SvetlanaIL <ilyasovasv91@yandex.ru>
2025-08-27 11:11:46 +0300

    add toggle flags and auto remove scripts

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,17 +1,22 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import {memo, useCallback} from 'react';
+import { memo, useCallback } from 'react';
 import cls from './ArticleDetailsPage.module.scss';
-import {useParams} from "react-router-dom";
-import {ArticleDetails} from "entities/Article";
-import {DynamicModuleLoader, ReducersList} from "shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
-import {Page} from "widgets/Page/ui/Page/Page";
-import {articleDetailsPageReducer} from "../../model/slices";
-import {ArticleDetailsPageHeader} from "pages/ArticleDetailsPage/ui/ArticleDetailsPageHeader/ArticleDetailsPageHeader";
-import {ArticleRecommendationsList} from "features/articleRecommendationsList";
-import {VStack} from "shared/ui/Stack";
-import {ArticleDetailsComments} from "pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments";
-import {ArticleRating} from "features/articleRating";
+import { useParams } from 'react-router-dom';
+import { ArticleDetails } from 'entities/Article';
+import {
+    DynamicModuleLoader,
+    ReducersList,
+} from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { Page } from 'widgets/Page/ui/Page/Page';
+import { articleDetailsPageReducer } from '../../model/slices';
+import { ArticleDetailsPageHeader } from 'pages/ArticleDetailsPage/ui/ArticleDetailsPageHeader/ArticleDetailsPageHeader';
+import { ArticleRecommendationsList } from 'features/articleRecommendationsList';
+import { VStack } from 'shared/ui/Stack';
+import { ArticleDetailsComments } from 'pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments';
+import { ArticleRating } from 'features/articleRating';
+import { toggleFeatures } from 'shared/lib/features';
+import { Card } from 'shared/ui/Card/Card';
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -28,19 +33,30 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
 
     if (!id) {
         return (
-            <Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+            <Page
+                className={classNames(cls.ArticleDetailsPage, {}, [className])}
+            >
                 {t('Статья не найдена')}
             </Page>
         );
     }
 
+    const articleRatingCard = toggleFeatures({
+        name: 'isArticleRatingEnabled',
+        on: () => <ArticleRating articleId={id} />,
+        off: () => <Card>{t('Оценка статей скоро появится!')}</Card>,
+    });
+
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-            <Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+            <Page
+                className={classNames(cls.ArticleDetailsPage, {}, [className])}
+            >
                 <VStack gap="16" max>
                     <ArticleDetailsPageHeader />
                     <ArticleDetails id={id} />
-                    <ArticleRating articleId={id} />
+                    {articleRatingCard}
+                    {/*<ArticleRating articleId={id} />*/}
                     <ArticleRecommendationsList />
                     <ArticleDetailsComments id={id} />
                 </VStack>
