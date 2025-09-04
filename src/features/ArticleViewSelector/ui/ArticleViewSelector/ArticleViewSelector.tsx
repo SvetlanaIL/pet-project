bf70131e740cfd9c2fcd9700be5c1faac432f21e
SvetlanaIL <ilyasovasv91@yandex.ru>
2025-09-04 12:05:20 +0300

    fix button in listbox, fix keys, fix forward ref

--- src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
+++ src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -15,7 +15,7 @@ import cls from './ArticleViewSelector.module.scss';
 import { ToggleFeatures, toggleFeatures } from 'shared/lib/features';
 import { Icon } from 'shared/ui/redesigned/Icon';
 import { Card } from 'shared/ui/redesigned/Card';
-import { HStack } from 'shared/ui/redesigned/Stack';
+import { HStack, VStack } from 'shared/ui/redesigned/Stack';
 import { ArticleView } from 'entities/Article/model/types/article';
 
 interface ArticleViewSelectorProps {
@@ -66,6 +66,7 @@ export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
                         {viewTypes.map((viewType) => (
                             <Icon
                                 clickable
+                                key={viewType.view}
                                 onClick={onClick(viewType.view)}
                                 Svg={viewType.icon}
                                 className={classNames('', {
